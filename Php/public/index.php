330ad8b8f6a7de52ffe72fe94b28bcb6a7752834
grudus <jakgruda@gmail.com>
2018-12-25 03:43:19 +0100

    Connect to mysql from php

--- Php/public/index.php
+++ Php/public/index.php
@@ -4,6 +4,16 @@
     <title>Sklep muzyczny</title>
 </head>
 <body>
-    <h1>Połączyłeś się z serwerem, brawo ty!</h1>
+<h1>Połączyłeś się z serwerem, brawo ty!</h1>
+
+
+<?php
+
+echo "Elo";
+include('../src/config/dbCredentials.php');
+
+$conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_DATABASE);
+
+?>
 </body>
 </html>
---
+++ Php/src/config/dbCredentials.php
@@ -0,0 +1,6 @@
+<?php
+
+$DB_HOST = getenv('DB_HOST');
+$DB_DATABASE = getenv('DB_DATABASE');
+$DB_USER = getenv('DB_USER');
+$DB_PASSWORD = getenv('DB_PASSWORD');
