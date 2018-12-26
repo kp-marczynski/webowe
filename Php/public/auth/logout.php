0bbd62aff9ea7323d25d08856a4f798d75f3dec0
grudus <jakgruda@gmail.com>
2018-12-26 21:47:01 +0100

    Add logout logic

---
+++ Php/public/auth/logout.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+session_unset();
+session_destroy();
+
+foreach ( $_COOKIE as $key => $value )
+{
+    setcookie( $key, '', 1, '/');
+}
+
+header("Location: /auth/login.php");
+exit();
