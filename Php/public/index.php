c8b959d0eb322a1aab0f18bcc6117ae1072c8670
grudus <jakgruda@gmail.com>
2018-12-27 22:59:54 +0100

    Create base landing page

--- Php/public/index.php
+++ Php/public/index.php
@@ -2,7 +2,40 @@
 include_once dirname($_SERVER["DOCUMENT_ROOT"]) . '/src/main/header.template.php';
 ?>
     <main class="under-nav">
-        <h3>Strona główna</h3>
+
+        <section class="landing-top">
+
+            <div class="landing-text">
+                <h2 class="landing-top-title">Nie przegap żadnego wydarzenia</h2>
+                <p class="landing-top-subtext">Dzięki Muzyka4zycie już nigdy nie ominie Cię żaden koncert w okolicy.
+                    <br/>
+                    Oglądaj swoich ulubionych muzyków na żywo i ciesz się otoczeniem ludzi, <br/>
+                    którzy czują to, co Ty.
+                </p>
+            </div>
+
+            <div class="landing-top-image-wrapper">
+                <img class="landing-image"
+                     src="/res/images/landing.svg"
+            </div>
+        </section>
+
+        <section class="landing-middle">
+
+            <div class="landing-text">
+                <h2 class="landing-top-title white">Ciesz się najlepszą muzyką</h2>
+                <p class="landing-top-subtext white">W Muzyka4zycie doskonale wiemy, jak ważną rolę pełni muzyka. <br/>
+                    Otocz się nią na koncercie swojego ulubionego zespołu.
+                </p>
+            </div>
+
+            <div class="landing-top-image-wrapper">
+                <img class="landing-image"
+                     src="/res/images/music.svg"
+            </div>
+
+        </section>
+
     </main>
 
 <?php
--- Php/src/main/header.template.php
+++ Php/src/main/header.template.php
@@ -23,6 +23,7 @@ if ($isRestrictedUrl && !isset($user)) {
     <link href="/styles/index.css" rel="stylesheet"/>
     <link href="/styles/header.css" rel="stylesheet"/>
     <link href="/styles/events.css" rel="stylesheet"/>
+    <link href="/styles/landing.css" rel="stylesheet"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="/res/images/favicon.ico" type="image/x-icon"/>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
