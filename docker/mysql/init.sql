471f829dc751101966ffd7a6053b476de2c4fc58
kp.marczynski <kp.marczynski@gmail.com>
2019-01-22 01:50:36 +0100

    style improvements

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -2,25 +2,25 @@ USE muzyka4zycie;
 
 CREATE TABLE IF NOT EXISTS users
 (
-  id              BIGINT PRIMARY KEY AUTO_INCREMENT,
-  email           VARCHAR(255) UNIQUE NOT NULL,
-  password        VARCHAR(255)        NOT NULL,
-  role            ENUM ('user', 'admin') DEFAULT 'user',
-  last_login_date DATETIME               DEFAULT CURRENT_TIMESTAMP,
-  token           VARCHAR(255)        NULL
+  id              BIGINT                  PRIMARY KEY AUTO_INCREMENT,
+  email           VARCHAR(255)            UNIQUE NOT NULL,
+  password        VARCHAR(255)            NOT NULL,
+  role            ENUM ('user', 'admin')  DEFAULT 'user',
+  last_login_date DATETIME                DEFAULT CURRENT_TIMESTAMP,
+  token           VARCHAR(255)            NULL
 );
 
 CREATE TABLE IF NOT EXISTS events
 (
-  id                          BIGINT PRIMARY KEY AUTO_INCREMENT,
+  id                          BIGINT                      PRIMARY KEY AUTO_INCREMENT,
   name                        VARCHAR(255)                NOT NULL,
   price                       REAL                        NOT NULL,
   place                       VARCHAR(255)                NOT NULL,
-  date                        DATETIME                             DEFAULT CURRENT_TIMESTAMP,
+  date                        DATETIME                    DEFAULT CURRENT_TIMESTAMP,
   short_info                  VARCHAR(255)                NULL,
   description                 TEXT                        NULL,
   image_url                   TEXT                        NULL,
-  number_of_available_tickets INTEGER                     NULL     DEFAULT 0,
+  number_of_available_tickets INTEGER                     NULL DEFAULT 0,
   created_by                  BIGINT                      NOT NULL,
   age_range                   ENUM ('ALL', 'ADULTS_ONLY') NOT NULL DEFAULT 'ALL',
   additional_info             VARCHAR(255)                NULL,
